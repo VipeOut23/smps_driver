3f15dbdd334d1a58f82cc3e811dcb465312f11de
Jonas Röger <jonas.roeger@tu-dortmund.de>
2020-04-06 22:46:13 +0200

    F_PWM correction

--- main.c
+++ main.c
@@ -13,15 +13,20 @@
 #define F_PWM_TGT 500000 // Not accurate, search for nearest clock divisor
 
 /* Determine PWM clock divisor based on F_PWM_TGT and F_CPU */
-#if F_CPU < F_PWM_TGT/0xFF
+#if F_CPU < F_PWM_TGT*0xFF
+#define F_PWM (F_CPU/0xFF)
 #define PWM_CK_DIV_BITS _BV(CS00)
-#elif F_CPU/8 < F_PWM_TGT/0xFF
+#elif F_CPU/8 < F_PWM_TGT*0xFF
+#define F_PWM ((F_CPU/8)/0xFF)
 #define PWM_CK_DIV_BITS _BV(CS01)
-#elif F_CPU/64 < F_PWM_TGT/0xFF
+#elif F_CPU/64 < F_PWM_TGT*0xFF
+#define F_PWM ((F_CPU/64)/0xFF)
 #define PWM_CK_DIV_BITS _BV(CS01) | _BV(CS00)
-#elif F_CPU/256 < F_PWM_TGT/0xFF
+#elif F_CPU/256 < F_PWM_TGT*0xFF
+#define F_PWM ((F_CPU/256)/0xFF)
 #define PWM_CK_DIV_BITS _BV(CS02)
 #else
+#define F_PWM ((F_CPU/1024)/0xFF)
 #define PWM_CK_DIV_BITS _BV(CS02) | _BV(CS00)
 #endif
 
@@ -44,9 +49,9 @@ static void pwm_init()
 int main()
 {
         uint16_t adc_res;
-        uint8_t gain;
+        uint8_t vx;
         size_t str_len;
-        char str[16];
+        char str[24];
 
         /* Set CK div to 2 -> 4MHz CK */
         cli();
@@ -80,8 +85,8 @@ int main()
                 OCR0B = OCR0A = adc_res;
 
                 /* Display info */
-                gain = (uint32_t) (100*adc_res)/0xFF;
-                str_len = sprintf(str, "%04u (%04u%%)\r\n", OCR0A, gain);
+                vx = (uint32_t) (100*adc_res)/0xFF;
+                str_len = sprintf(str, "%03d%% @ %ldHz\r\n", vx, F_PWM);
                 uart_try_puts(str, str_len);
         }
 }
